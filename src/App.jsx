74f07f22b518851e0bb9a258cd7d12348a5d045b
husnain3964 <husnain3964@gmail.com>
2025-09-17 18:42:37 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navber from "./components/navber/Navber.jsx";
 import Hero from "./components/hero/hero.jsx";
 import About from "./components/about/About.jsx";
-import "./app.css";
+import "./App.css";
 import Services from "./components/services/services.jsx";
 import Mywork from "./components/mywork/Mywork.jsx";
 import Contact from "./components/contact/Contact.jsx";
